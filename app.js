b03ef029b2cb6c217318a7c1c3c2032e20c919f6
Dion Short <dion.short@outlook.com>
2026-06-28 20:03:10 +0100

    Fix CORS origin URL by removing trailing slash

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://wtwr-frontend.vercel.app/'
+  origin: 'https://wtwr-frontend.vercel.app'
 }));
 
 // enable request logger
